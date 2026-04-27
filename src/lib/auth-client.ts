bd01d15a3c989bbaa70e7d77942f95247fb15bce
Raj3460 <sarkarrajkumar3460@gmail.com>
2026-04-28 02:01:30 +0600

    added fixt craditiial

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,20 +1,20 @@
-import { env } from "@/env"
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
-    credentials: "include", // Include cookies for authentication
-})
-
-
 // import { env } from "@/env"
 // import { createAuthClient } from "better-auth/react"
-
 // export const authClient = createAuthClient({
-//   baseURL: env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
-//   fetchOptions: {
-//     credentials: "include",
-//   },
+//     /** The base URL of the server (optional if you're using the same domain) */
+//     baseURL: env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
+//     credentials: "include", // Include cookies for authentication
 // })
+
+
+import { env } from "@/env"
+import { createAuthClient } from "better-auth/react"
+
+export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
+  fetchOptions: {
+    credentials: "include",
+  },
+})
 
 // export const { signIn, signUp, signOut, useSession } = authClient;
